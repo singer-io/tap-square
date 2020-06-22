2448db66aeed8d16921ee5f5afd1748bc19c824e
Jacob Baca <52418765+jacobrobertbaca@users.noreply.github.com>
2020-06-22 12:02:11 -0400

    Add sync for discounts (#5)

--- tap_square/streams.py
+++ tap_square/streams.py
@@ -51,7 +51,34 @@ class Categories:
         return state
 
 
+class Discounts:
+    tap_stream_id = 'discounts'
+    key_properties = ['id']
+    replication_method = 'INCREMENTAL'
+    valid_replication_keys = ['updated_at']
+    replication_key = 'updated_at'
+
+    def sync(self, client, state, start_time, bookmarked_cursor):
+        max_updated_at = start_time
+
+        for page, cursor in client.get_catalog_discounts(start_time, bookmarked_cursor):
+
+            for item in page:
+                singer.write_record(self.tap_stream_id, item)
+                if item['updated_at'] > max_updated_at:
+                    max_updated_at = item['updated_at']
+
+            state = singer.write_bookmark(state, self.tap_stream_id, 'cursor', cursor)
+            state = singer.write_bookmark(state, self.tap_stream_id, self.replication_key, max_updated_at)
+            singer.write_state(state)
+
+        state = singer.clear_bookmark(state, self.tap_stream_id, 'cursor')
+
+        return state
+
+
 STREAMS = {
     'items': Items,
     'categories': Categories,
+    'discounts': Discounts
 }
