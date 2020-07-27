370d64c4b838afd676c7d3bda6f4a7a96f6a5c69
Andy Lu <andy@stitchdata.com>
2020-07-27 13:24:25 +0000

    Remove Payments cache

--- tests/test_client.py
+++ tests/test_client.py
@@ -75,8 +75,8 @@ class TestClient(SquareClient):
         elif stream == 'refunds':
             return [obj for page, _ in self.get_refunds(start_date, None) for obj in page]
         elif stream == 'payments':
-            if not self.PAYMENTS:
-                self.PAYMENTS = [obj for page, _ in self.get_payments(start_date, None) for obj in page]
+            #if not self.PAYMENTS:
+            self.PAYMENTS = [obj for page, _ in self.get_payments(start_date, None) for obj in page]
             return self.PAYMENTS
         elif stream == 'modifier_lists':
             return [obj for page, _ in self.get_catalog('MODIFIER_LIST', start_date, None) for obj in page]
