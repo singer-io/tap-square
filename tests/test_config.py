ff8ff424505f5e347d3e3ed57e1165f366fd3851
Kyle Speer <kspeer@stitchdata.com>
2020-08-18 18:19:11 -0400

    look at subdirs

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,6 +5,8 @@ import os.path
 # Read in the filenames of the tests/ directroy
 cwd = os.getcwd()
 print("contents of cwd: {}".format(os.listdir(cwd)))
+print("contents of tests: {}".format(os.listdir(cwd + '/tests')))
+print("contents of tap-square: {}".format(os.listdir(cwd + '/tap-square')))
 files = [
     name for name in os.listdir('/tap-square/tests/')
     if 'test' == name[:4] and '.py' == name[-3:] and \
