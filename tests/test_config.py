4cc36d2bd998db6e887d605452270360f384cd19
Kyle Speer <kspeer@stitchdata.com>
2020-08-18 18:24:30 -0400

    for real this time

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,18 +3,14 @@ import re
 import os.path
 
 # Read in the filenames of the tests/ directroy
-cwd = os.getcwd()
-print("contents of cwd: {}".format(os.listdir(cwd)))
-print("contents of tests: {}".format(os.listdir(cwd + '/tests')))
-print("contents of tap-square: {}".format(os.listdir(cwd + '/tap-square')))
 files = [
-    name for name in os.listdir('/tap-square/tests/')
+    name for name in os.listdir(cwd + '/tests')
     if 'test' == name[:4] and '.py' == name[-3:] and \
     name not in ['test_client.py', 'test_config.py']
 ]
 
 # Read in the circle config
-with open("../tap-square/.circleci/config.yml", "r") as config:
+with open(cwd + "/.circleci/config.yml", "r") as config:
     contents = config.read()
 runs = contents.replace(' ', '').replace('\n', '').split('-run:') # separate into run blocks
 
