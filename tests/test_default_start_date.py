715870097e34b314a14d6a3bd65aadfd1b028fee
Asaf Erlich <aerlich@talend.com>
2020-09-04 16:19:19 -0400

    Remove unnecessarily syncing random days between 1 year and now in test_default_start_date

--- tests/test_default_start_date.py
+++ tests/test_default_start_date.py
@@ -61,17 +61,6 @@ class TestSquareStartDateDefault(TestSquareBaseParent.TestSquareBase):
         self.default_start_date_test(DataType.DYNAMIC, self.testable_streams_dynamic().intersection(self.production_streams()))
         self.default_start_date_test(DataType.STATIC, self.testable_streams_static().intersection(self.production_streams()))
 
-        days_prior = random.randint(7, 358) # 1 week to 57 weeks
-
-        # Testing start date prior to default
-        self.START_DATE = dt.strftime(default_start_date - timedelta(days=days_prior), self.START_DATE_FORMAT)
-        self.set_environment(self.SANDBOX)
-        self.default_start_date_test(DataType.DYNAMIC, self.testable_streams_dynamic().intersection(self.sandbox_streams()))
-        self.default_start_date_test(DataType.STATIC, self.testable_streams_static().intersection(self.sandbox_streams()))
-        self.set_environment(self.PRODUCTION)
-        self.default_start_date_test(DataType.DYNAMIC, self.testable_streams_dynamic().intersection(self.production_streams()))
-        self.default_start_date_test(DataType.STATIC, self.testable_streams_static().intersection(self.production_streams()))
-
     def default_start_date_test(self, data_type, testable_streams):
         streams_without_data = self.untestable_streams()
 
