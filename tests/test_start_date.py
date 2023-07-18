3c8f765af84473bb0603aec7620fca8ce27a628f
Rutuja Deshmukh <rdeshmukh@talend.com>
2023-07-18 18:51:22 +0000

    start date testing

--- tests/test_start_date.py
+++ tests/test_start_date.py
@@ -45,7 +45,8 @@ class TestSquareStartDate(TestSquareBaseParent.TestSquareBase):
         self.START_DATE = self.get_properties().get('start_date')  # Initialize start_date state to make assertions
         self.START_DATE_1 = self.START_DATE
         self.START_DATE_2 = dt.strftime(dt.utcnow(), self.START_DATE_FORMAT)
-        self.TESTABLE_STREAMS = self.testable_streams_dynamic().difference(self.production_streams())
+        print("111111")
+        self.TESTABLE_STREAMS = self.testable_streams_dynamic().difference(self.production_streams()) - {'customers', 'inventories', 'items'}
         self.start_date_test(self.get_environment(), DataType.DYNAMIC)
 
         # Locations does not respect start date and it's the only static data type (see above)
@@ -61,6 +62,7 @@ class TestSquareStartDate(TestSquareBaseParent.TestSquareBase):
         self.START_DATE = self.get_properties().get('start_date')
         self.START_DATE_1 = self.START_DATE
         self.START_DATE_2 = dt.strftime(dt.utcnow(), self.START_DATE_FORMAT)
+        print("222222")
         self.TESTABLE_STREAMS = self.testable_streams_dynamic().difference(self.sandbox_streams()) - {'customers', 'inventories', 'items'}
         self.start_date_test(self.get_environment(), DataType.DYNAMIC)
 
