f85f0ce9a22beaf48dbaaa4fd80cc34ff40996fc
Rutuja Deshmukh <rdeshmukh@talend.com>
2023-07-20 07:30:34 +0000

    version changes in setup.py and CHANGELOG.md

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-square',
-      version='1.3.1',
+      version='2.0.0',
       description='Singer.io tap for extracting data from the Square API',
       author='Stitch',
       url='http://singer.io',
